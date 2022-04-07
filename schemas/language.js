539750ddbffd595b49829acfea7bbcd860c93c36
Sudeep Gurung <webtree22@gmail.com>
2022-04-07 13:13:35 +0545

    wip]

--- schemas/language.js
+++ schemas/language.js
@@ -1,12 +1,12 @@
 export default {
-  name: "language",
-  title: "Language",
-  type: "document",
+  name: 'language',
+  title: 'Language',
+  type: 'document',
   fields: [
     {
-      name: "name",
-      title: "Name",
-      type: "string",
-    },
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }
   ],
 };
